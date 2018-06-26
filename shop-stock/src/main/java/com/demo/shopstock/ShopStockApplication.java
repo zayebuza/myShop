3f87bb6780bae2c44bd401ad8a2185de9ca351ea
shanks <miaodongbiao@ucredit.com>
2018-06-26 13:43:23 +0800

    初始化订单模块  库存模块

---
+++ shop-stock/src/main/java/com/demo/shopstock/ShopStockApplication.java
@@ -0,0 +1,12 @@
+package com.demo.shopstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopStockApplication.class, args);
+    }
+}
